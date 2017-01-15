df319586cbcd08ca745c32eca78a75b650f60ce7
bushmb <bushmb@gmail.com>
2017-01-14 18:43:57 -0800

    first commit, BlogPosts API. CRUD working

---
+++ server.js
@@ -0,0 +1,87 @@
+/* jshint esversion: 6 */
+
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const {BlogPosts} = require('./models');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+// log the http layer
+app.use(morgan('common'));
+
+// we're going to add some items to BlogPosts
+// so there's some data to look at
+BlogPosts.create('My Day at the Beach', 'Today, Saturday, I had a great day at the beach. It was sunny.', 'William Shakespeare', '2017-01-13');
+BlogPosts.create('Another Day at the Beach', 'This was the second time I went to the beach. It was also sunny. I do not think it rains', 'William Shakespeare', '2017-01-14');
+
+// when the root of this router is called with GET, return
+// all current BlogPosts items
+app.get('/blog-posts', (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+app.post('/blog-posts', jsonParser, (req, res) => {
+  // ensure `title`, `cotent`, `author`, `publishDate` are in request body
+  const requiredFields = ['title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+  res.status(201).json(item);
+});
+
+// when PUT request comes in with updated item, ensure has
+// required fields. also ensure that item id in url path, and
+// item id in updated item object match. if problems with any
+// of that, log error and send back status code 400. otherwise
+// call `ShoppingList.update` with updated item.
+app.put('/blog-posts/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', 'publishDate', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post item \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).json(updatedItem);
+});
+
+// when DELETE request comes in with an id in path,
+// try to delete that item from BlogPosts.
+app.delete('/blog-posts/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog post item \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
